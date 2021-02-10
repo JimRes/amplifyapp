1de5820434477aa48c65e765a1a5593a7b8bff81
Jim Restivo <Jim.Restivo@att.net>
2021-02-09 21:41:40 -0500

    testing changes to the main page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Pick your benefits for 2021 open enrollment</p>
         <a
           className="App-link"
           href="https://reactjs.org"
